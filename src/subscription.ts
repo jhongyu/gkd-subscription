38ea328810900b37160916e82198f18074dc5eb6
jhongyu <jhy172691619@gmail.com>
2026-05-31 16:29:56 +0800

    更新版本号

--- src/subscription.ts
+++ src/subscription.ts
@@ -6,7 +6,7 @@ import globalGroups from './globalGroups';
 export default defineGkdSubscription({
   id: 55676,
   name: 'gkd-subscription',
-  version: 2026.0501,
+  version: 20260531,
   author: 'jhongyu',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/jhongyu/gkd-subscription',
